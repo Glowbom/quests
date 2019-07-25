08344cc35d0d9f9bf0b1fcbd130a45a29b525bab
Jacob Ilin <me@jacobilin.com>
2019-07-25 02:53:26 -0600

    Showing all elements

--- textadventures/Assets/MagicCodingTool/QuestEditor/Editor/QuestEditor.cs
+++ textadventures/Assets/MagicCodingTool/QuestEditor/Editor/QuestEditor.cs
@@ -11,6 +11,7 @@ using UnityEditor;
 public class QuestEditor : EditorWindow
 {   
     private int tab = 0;
+    private int tabElements = 0;
 
     [MenuItem("Window/Glowbom/Quests")]
     public static void ShowWindow() {
@@ -79,94 +80,172 @@ public class QuestEditor : EditorWindow
 
         EditorGUILayout.Space();
 
-        GUILayout.Label("Main Quest", EditorStyles.label);
+        GUILayout.Label("Project 1", EditorStyles.label);
 
         EditorGUILayout.Space();
 
-        GUILayout.Label("Element 1", EditorStyles.label);
+        tabElements = GUILayout.Toolbar (tabElements, new string[] {"All", "1: 0"});
+        switch (tabElements) {
+            case 0:
+                EditorGUILayout.Space();
 
-        EditorGUILayout.TextField("Title", "0");
-        EditorGUILayout.TextField("Text", "hello");
+                if (GUILayout.Button("1: 0")) {
+                    // login code here
+                }
 
-        EditorGUILayout.Space();
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("Insert")) {
+                    // add button code here
+                }
 
-        GUILayout.Label("Button 1", EditorStyles.label);
+                if (GUILayout.Button("Remove")) {
+                    // remove button code here
+                }
+                GUILayout.EndHorizontal();
 
-        EditorGUILayout.TextField("Title", "Desicion 1");
-        EditorGUILayout.TextField("Go To", "0");
+                EditorGUILayout.Space();
 
-        GUILayout.BeginHorizontal();
+                if (GUILayout.Button("2: Welcome")) {
+                    // login code here
+                }
+
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("Insert")) {
+                    // add button code here
+                }
+
+                if (GUILayout.Button("Remove")) {
+                    // remove button code here
+                }
+                GUILayout.EndHorizontal();
+
+                EditorGUILayout.Space();
+
+                if (GUILayout.Button("3: No Way")) {
+                    // login code here
+                }
+
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("Insert")) {
+                    // add button code here
+                }
+
+                if (GUILayout.Button("Remove")) {
+                    // remove button code here
+                }
+                GUILayout.EndHorizontal();
+
+                EditorGUILayout.Space();
+
+                if (GUILayout.Button("4: 3")) {
+                    // login code here
+                }
+
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("Add")) {
+                    // add button code here
+                }
+
+                if (GUILayout.Button("Remove")) {
+                    // remove button code here
+                }
+                GUILayout.EndHorizontal();
+
+                EditorGUILayout.Space();
+
+                EditorGUILayout.Space();
+            break;
+            case 1:
+                EditorGUILayout.Space();
+
+                GUILayout.Label("Element 1", EditorStyles.label);
+
+                EditorGUILayout.TextField("Title", "0");
+                EditorGUILayout.Space();
+                EditorGUILayout.TextArea("hello", GUILayout.Height(100));
+
+                EditorGUILayout.Space();
+
+                GUILayout.Label("Button 1", EditorStyles.label);
+
+                EditorGUILayout.TextField("Title", "Desicion 1");
+                EditorGUILayout.TextField("Go To", "1");
+
+                GUILayout.BeginHorizontal();
         
-        if (GUILayout.Button("Add")) {
-            // add button code here
-        }
+                if (GUILayout.Button("Insert")) {
+                    // add button code here
+                }
 
-        if (GUILayout.Button("Remove")) {
-            // remove button code here
-        }
+                if (GUILayout.Button("Remove")) {
+                    // remove button code here
+                }
 
-        GUILayout.EndHorizontal();
+                GUILayout.EndHorizontal();
 
-        EditorGUILayout.Space();
+                EditorGUILayout.Space();
 
-        GUILayout.Label("Button 2", EditorStyles.label);
+                GUILayout.Label("Button 2", EditorStyles.label);
 
-        EditorGUILayout.TextField("Title", "Desicion 2");
-        EditorGUILayout.TextField("Go To", "1");
+                EditorGUILayout.TextField("Title", "Desicion 2");
+                EditorGUILayout.TextField("Go To", "1");
 
-        GUILayout.BeginHorizontal();
+                GUILayout.BeginHorizontal();
         
-        if (GUILayout.Button("Add")) {
-            // add button code here
-        }
+                if (GUILayout.Button("Insert")) {
+                    // add button code here
+                }
 
-        if (GUILayout.Button("Remove")) {
-            // remove button code here
-        }
+                if (GUILayout.Button("Remove")) {
+                    // remove button code here
+                }
 
-        GUILayout.EndHorizontal();
+                GUILayout.EndHorizontal();
 
-        EditorGUILayout.Space();
+                EditorGUILayout.Space();
 
-        GUILayout.Label("Button 3", EditorStyles.label);
+                GUILayout.Label("Button 3", EditorStyles.label);
 
-        EditorGUILayout.TextField("Title", "Desicion 3");
-        EditorGUILayout.TextField("Go To", "3");
+                EditorGUILayout.TextField("Title", "Desicion 3");
+                EditorGUILayout.TextField("Go To", "3");
 
-        GUILayout.BeginHorizontal();
+                GUILayout.BeginHorizontal();
         
-        if (GUILayout.Button("Add")) {
-            // add button code here
-        }
+                if (GUILayout.Button("Insert")) {
+                    // add button code here
+                }
 
-        if (GUILayout.Button("Remove")) {
-            // remove button code here
-        }
+                if (GUILayout.Button("Remove")) {
+                    // remove button code here
+                }
 
-        GUILayout.EndHorizontal();
+                GUILayout.EndHorizontal();
 
-        EditorGUILayout.Space();
+                EditorGUILayout.Space();
 
-        GUILayout.Label("Button 4", EditorStyles.label);
+                GUILayout.Label("Button 4", EditorStyles.label);
 
-        EditorGUILayout.TextField("Title", "Desicion 4");
-        EditorGUILayout.TextField("Go To", "0");
+                EditorGUILayout.TextField("Title", "Desicion 4");
+                EditorGUILayout.TextField("Go To", "0");
 
-        GUILayout.BeginHorizontal();
+                GUILayout.BeginHorizontal();
         
-        if (GUILayout.Button("Add")) {
-            // add button code here
+                if (GUILayout.Button("Add")) {
+                    // add button code here
+                }
+
+                if (GUILayout.Button("Remove")) {
+                    // remove button code here
+                }
+
+                GUILayout.EndHorizontal();
+                EditorGUILayout.Space();
+            break;
         }
-
-        if (GUILayout.Button("Remove")) {
-            // remove button code here
-        }
-
-        GUILayout.EndHorizontal();
-
-        EditorGUILayout.Space();
 
         GUILayout.EndVertical();
+
+        /// end of elements
 
         EditorGUILayout.Space();
 
@@ -176,7 +255,7 @@ public class QuestEditor : EditorWindow
 
         EditorGUILayout.Space();
 
-        GUILayout.Label("More Quests", EditorStyles.label);
+        GUILayout.Label("Projects", EditorStyles.label);
 
         EditorGUILayout.Space();
 
