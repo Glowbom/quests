bf3252b8efb15a544708c2498de9c1f3fcbb72e3
Jacob Ilin <me@jacobilin.com>
2019-07-28 17:26:30 -0600

    Loading quest logic

--- textadventures/Assets/MagicCodingTool/Scripts/QuestLoader.cs
+++ textadventures/Assets/MagicCodingTool/Scripts/QuestLoader.cs
@@ -2,6 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Created on Sun Jul 28 2019
+ *
+ * Copyright (c) 2019 Glowbom
+ */
 public class QuestLoader
 {
+    public Logic logic = null;
+
+    public void load() {
+        var textAsset = Resources.Load ("Data/MainQuest") as TextAsset;
+        logic = JsonUtility.FromJson<Logic> (textAsset.text);
+    }
+
+    public void save() {
+        
+    }
 }
