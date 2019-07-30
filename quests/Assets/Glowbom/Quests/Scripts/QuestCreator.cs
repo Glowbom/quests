62d452f67b82621d8828c9e820619cc45889b77d
Jacob Ilin <me@jacobilin.com>
2019-07-30 11:00:57 -0600

    Quest creator

---
+++ quests/Assets/Glowbom/Quests/Scripts/QuestCreator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestCreator : MonoBehaviour
+{
+    QuestLoader loader = new QuestLoader();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        loader.load();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
