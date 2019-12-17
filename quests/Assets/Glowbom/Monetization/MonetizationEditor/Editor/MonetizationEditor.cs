8fc528990c00014a993e7f744e8da4ab023459a0
Jacob Ilin <jacob@glowbom.com>
2019-12-16 17:44:51 -0700

    Add ads support to Quests

---
+++ quests/Assets/Glowbom/Monetization/MonetizationEditor/Editor/MonetizationEditor.cs
@@ -0,0 +1,28 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MonetizationEditor : EditorWindow
+{
+    static MonetizationEditor()
+    {
+    }
+
+    [MenuItem("Window/Glowbom/Monetization")]
+    public static void ShowWindow()
+    {
+        GetWindow<QuestEditor>("Monetization");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Glowbom", EditorStyles.boldLabel);
+
+        GUILayout.Label("Monetization", EditorStyles.label);
+
+        EditorGUILayout.Space();
+    }
+}
+#endif
