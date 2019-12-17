74dc77823ace95befd10915fd0b96931f84ede88
Jacob Ilin <jacob@glowbom.com>
2019-12-16 18:02:17 -0700

    monetization classes

---
+++ quests/Assets/Glowbom/Monetization/MonetizationEditor/Monetization.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Created on Mon Dec 16 2019
+ *
+ * Copyright (c) 2019 Glowbom.
+ */
+[System.Serializable]
+public class Monetization : MonoBehaviour
+{
+    public string androidBanner;
+    public string androidInterstitial;
+    public string androidAppId;
+
+    public string iOSBanner;
+    public string iOSInterstitial;
+    public string iOSAppId;
+}
