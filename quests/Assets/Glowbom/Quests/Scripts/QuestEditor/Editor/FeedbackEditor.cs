c6e67eb0608c07f7c0fbce5fe2d079e2390b2bd0
Jacob Ilin <me@jacobilin.com>
2020-01-06 20:17:42 +0400

    FeedbackEditor

---
+++ quests/Assets/Glowbom/Quests/Scripts/QuestEditor/Editor/FeedbackEditor.cs
@@ -0,0 +1,39 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+public class FeedbackEditor : EditorWindow
+{
+    public static string link = null;
+
+    static FeedbackEditor()
+    {       
+
+    }
+
+    [MenuItem("Window/Glowbom/Feedback")]
+    public static void ShowWindow()
+    {
+        GetWindow<FeedbackEditor>("Feedback");
+    }
+
+    private void OnGUI()
+    {
+
+        GUILayout.Label("Glowbom", EditorStyles.boldLabel);
+
+        GUILayout.Label("Feedback", EditorStyles.label);
+
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Load"))
+        {
+            // get data from forms here
+        }
+
+        EditorGUILayout.Space();
+
+        link = EditorGUILayout.TextField("Link", link);
+    }
+}
+#endif
