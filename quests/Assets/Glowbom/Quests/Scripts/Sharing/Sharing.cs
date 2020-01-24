43dc30ad7e023b714a3d5c90e5864aca9c431b24
Jacob Ilin <me@jacobilin.com>
2020-01-24 16:24:24 +0400

    Sharing on Android

--- quests/Assets/Glowbom/Quests/Scripts/Sharing/Sharing.cs
+++ quests/Assets/Glowbom/Quests/Scripts/Sharing/Sharing.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Collections;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 /*
@@ -13,10 +14,49 @@ public class Sharing : MonoBehaviour
     [DllImport("__Internal")]
     private static extern void _shareMessage(string message, string url);
 
-    public static void shareMessage(string message, string url = "")
+    public void shareMessage(string message, string url = "")
     {
         _shareMessage(message, url);
     }
+
+#elif UNITY_ANDROID
+
+    public void shareMessage(string message, string url = "")
+    {
+        StartCoroutine(shareMessageInAnroid(message, url));
+    }
+
+    public IEnumerator shareMessageInAnroid (string message, string url) {
+
+		if (!Application.isEditor) {
+			//Create intent for action send
+			AndroidJavaClass intentClass = 
+				new AndroidJavaClass ("android.content.Intent");
+			AndroidJavaObject intentObject = 
+				new AndroidJavaObject ("android.content.Intent");
+			intentObject.Call<AndroidJavaObject> 
+				("setAction", intentClass.GetStatic<string> ("ACTION_SEND"));
+
+			//put text and subject extra
+			intentObject.Call<AndroidJavaObject> ("setType", "text/plain");
+			intentObject.Call<AndroidJavaObject> 
+				("putExtra", intentClass.GetStatic<string> ("EXTRA_SUBJECT"), "");
+			intentObject.Call<AndroidJavaObject> 
+				("putExtra", intentClass.GetStatic<string> ("EXTRA_TEXT"), message + " " + url);
+
+			//call createChooser method of activity class
+			AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+			AndroidJavaObject currentActivity = 
+				unity.GetStatic<AndroidJavaObject> ("currentActivity");
+			AndroidJavaObject chooser = 
+				intentClass.CallStatic<AndroidJavaObject> 
+				("createChooser", intentObject, "Share your high score");
+			currentActivity.Call ("startActivity", chooser);
+		}
+
+		yield return new WaitUntil (() => true);
+	}
+	
 
 #else
 
