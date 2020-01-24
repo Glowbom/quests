0d51ae5fa2b424d158e316bd3f253043953efc20
Jacob Ilin <me@jacobilin.com>
2020-01-24 16:34:48 +0400

    Android sharing example

---
+++ quests/Assets/Glowbom/Quests/Scripts/Sharing/Android/AndroidSharing.java
@@ -0,0 +1,11 @@
+﻿// Java code for sharing
+public class AndroidSharing {
+    public void share(string message, string url) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "");
+        sendIntent.putExtra(Intent.EXTRA_TEXT, message + " " + url);
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, "Share Via"));
+    }
+}
