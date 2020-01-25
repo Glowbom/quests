2537f0ec70813b71e27705271cb0e95dc2b95c92
Jacob Ilin <me@jacobilin.com>
2020-01-25 20:24:51 +0400

    Feedback loaded doesn't have to be a mono behavior class

--- quests/Assets/Glowbom/Quests/Scripts/FeedbackLoader.cs
+++ quests/Assets/Glowbom/Quests/Scripts/FeedbackLoader.cs
@@ -1,18 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.IO;
 
-public class FeedbackLoader : MonoBehaviour
+public class FeedbackLoader
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
