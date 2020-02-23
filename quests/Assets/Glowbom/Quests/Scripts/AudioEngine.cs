bccf6f65f2704e7bc1b67a3db227095fe9f821de
Jacob Ilin <me@jacobilin.com>
2020-02-23 10:02:54 +0400

    Audio engine created

---
+++ quests/Assets/Glowbom/Quests/Scripts/AudioEngine.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Created on Wed Feb 23 2020
+ *
+ * Copyright (c) 2020 Glowbom.
+ */
+public class AudioEngine : MonoBehaviour
+{
+    public AudioSource correctAudioSource;
+    public AudioSource incorrectAudioSource;
+
+    public void correct()
+    {
+        correctAudioSource.Play();
+    }
+
+    public void incorrect()
+    {
+        incorrectAudioSource.Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
