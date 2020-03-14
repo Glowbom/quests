24aba1f5753b969886f03296c21a35a88b54e991
Jacob Ilin <me@jacobilin.com>
2020-03-14 01:36:24 -0700

    Audio engine changes

--- quests/Assets/Glowbom/Quests/Scripts/AudioEngine.cs
+++ quests/Assets/Glowbom/Quests/Scripts/AudioEngine.cs
@@ -43,6 +43,7 @@ public class AudioEngine : MonoBehaviour
     void Start()
     {
         audioMap = new Dictionary<long, AudioSource>();
+        audioMap.Clear();
     }
 
     // Update is called once per frame
