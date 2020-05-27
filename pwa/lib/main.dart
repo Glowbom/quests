70271f4967d5cc2f3bd949596acc55c5b48821e4
Jacob Ilin <me@jacobilin.com>
2020-05-26 19:03:35 -0600

    Adding Scaffold

--- pwa/lib/main.dart
+++ pwa/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Quests());
-}
+void main() => runApp(Quests());
 
 class Quests extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Glowbom Quests'),);
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(
+        title: Text('Glowbom Quests'),
+      ),
+      body: Text('Welcome to Glowbom Quests'),
+    ),);
   }
 }
