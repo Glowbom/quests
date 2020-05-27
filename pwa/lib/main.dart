dbca564867bcdd5ba87a1b6c041cdc582f860195
Jacob Ilin <me@jacobilin.com>
2020-05-26 19:47:39 -0600

    Adding quiz buttons

--- pwa/lib/main.dart
+++ pwa/lib/main.dart
@@ -5,11 +5,33 @@ void main() => runApp(Quests());
 class Quests extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(
-        title: Text('Glowbom Quests'),
+    var questions = [
+      'What is the supreme law of the land?',
+      'What does the Constitution do?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Glowbom Quests'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('Question 1'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
-      body: Text('Welcome to Glowbom Quests'),
-    ),);
+    );
   }
 }
